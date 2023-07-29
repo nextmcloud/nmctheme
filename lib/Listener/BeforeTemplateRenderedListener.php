8a4356992b14c79a9140837b65e68b14a412c3ca
Bernd.Rederlechner@t-systems.com <bernd.rederlechner@t-systems.com>
2023-07-29 16:56:46 +0200

    Document and clean optional unused listener

--- lib/Listener/BeforeTemplateRenderedListener.php
+++ lib/Listener/BeforeTemplateRenderedListener.php
@@ -11,47 +11,19 @@ declare(strict_types=1);
  */
 namespace OCA\NMCTheme\Listener;
 
-use OCP\IURLGenerator;
+use OCP\AppFramework\Http\TemplateResponse;
 use OCP\EventDispatcher\Event;
 use OCP\EventDispatcher\IEventListener;
-use OCP\AppFramework\Http\TemplateResponse;
-use OCA\Theming\Service\ThemesService;
-use OCA\NMCTheme\Themes\TeleNeoWebFont;
+use OCP\IURLGenerator;
 
 class BeforeTemplateRenderedListener implements IEventListener {
 	private IURLGenerator $urlGenerator;
-    private ThemesService $themesService;
-    private ThemeInjectionService $themeInjectionService;
-	private TeleNeoWebFont $teleNeoWebFont;
 
 	public function __construct(
-		IURLGenerator $urlGenerator,
-        ThemesService $themesService,
-		TeleNeoWebFont $teleNeoWebFont) {
-        $this->urlGenerator = $urlGenerator;
-		$this->themesService = $themesService;
-		$this->teleNeoWebFont = $teleNeoWebFont;
+		IURLGenerator $urlGenerator
+	) {
+		$this->urlGenerator = $urlGenerator;
 	}
-
-    /**
-	 * Inject theme header into anonymous pages without user relation
-	 *
-	 * @param string $themeId the theme ID
-	 * @param bool $plain request the :root syntax
-	 * @param string $media media query to use in the <link> element
-     */
-    private function addPublicThemeHeader(string $themeId, bool $plain = true, string $media = null) {
-		$linkToCSS = $this->urlGenerator->linkToRoute('theming.Theming.getThemeStylesheet', [
-			'themeId' => $themeId,
-			'plain' => $plain
-		]);
-		\OCP\Util::addHeader('link', [
-			'rel' => 'stylesheet',
-			'media' => $media,
-			'href' => $linkToCSS
-		]);
-	}
-
 
 	/**
 	 * The registration of Magenta themes is only done
@@ -59,16 +31,9 @@ class BeforeTemplateRenderedListener implements IEventListener {
 	 * application bootstrapping).
 	 */
 	public function handle(Event $event): void {
-		// always enable TeleNeoWebFonts if this theme is installed
-        if ($event->getResponse()->getRenderAs() === TemplateResponse::RENDER_AS_USER) {
-            $this->themesService->enableTheme($this->teleNeoWebFont);
-        } else {
-            // fix Telekom font theme always
-            $this->addPublicThemeHeader($this->teleNeoWebFont->getId());
-
-            // TODO: we can add dedicated theme css for anonymous renderings directly or
-            // as theme injection
-            \OCP\Util::addStyle("nmctheme", "nmcdefault");       
-        }
+		// you can add extra styles depending on situation
+		// if ($event->getResponse()->getRenderAs() === TemplateResponse::RENDER_AS_USER) {
+		//     \OCP\Util::addStyle("nmctheme", "some_extra_xxx");
+		// }
 	}
 }
