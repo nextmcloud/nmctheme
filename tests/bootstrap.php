d07e66a2d9e20743535f23d51d03cb8346bb83a7
Bernd.Rederlechner@t-systems.com <bernd.rederlechner@t-systems.com>
2023-07-29 16:57:12 +0200

    Enable phpunit for reusable fixture with Psr4 path

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,5 +2,6 @@
 
 require_once __DIR__ . '/../../../tests/bootstrap.php';
 
+\OC::$composerAutoloader->addPsr4('OCA\\NMCTheme\\Tests\\', dirname(__FILE__) . '/unit/', true);
 \OC_App::loadApp('nmctheme');
 OC_Hook::clear();
