e837da1000d49aa5abadb7a27f3234e20e4456e4
Bernd.Rederlechner@t-systems.com <bernd.rederlechner@t-systems.com>
2023-08-14 17:33:13 +0200

    Add dynamic javascript header (to be refined)

--- js/nmcfooter.js
+++ js/nmcfooter.js
@@ -1,38 +1,29 @@
-class TeleBrandFooter extends HTMLElement {
-    connectedCallback() {
-        this.innerHTML = `
-        <footer id="telekom-footer" role="contentinfo">
-            <div class="telekom-footer-minimal">
-                <div id="notice">
-                    (C) Telekom Deutschland GmbH
-                </div>
-                <ul id="navigation">
-                    <li><a href="https://static.magentacloud.de/licences/webui.htm" target="_blank" rel="noreferrer noopener">Open Source Lizenzen</a>
-                    <li><a href="http://www.telekom.de/impressum" target="_blank" rel="noreferrer noopener">Impressum</a></li>
-                    <li><a href="https://static.magentacloud.de/Datenschutz" target="_blank" rel="noreferrer noopener">Datenschutz</a>
-                    </li>
-                    <li><a href="https://cloud.telekom-dienste.de/hilfe" target="_blank" rel="noreferrer noopener">Hilfe und FAQ</a></li>
-                </ul>
-            </div>
-        </footer>
-        `;
-    }
-}
-
-window.customElements.define('telekom-footer-element', TeleBrandFooter);
-
 var footerElement = document.querySelector('body footer');
 if ( footerElement === null ) {
     // add footer tag
-    document.body.appendChild(new TeleBrandFooter());
-} else {
-    //replace footer tag
-    document.body.replaceChild(new TeleBrandFooter(), footerElement);
-}
+    footerElement = document.createElement('footer');
+    document.body.appendChild(footerElement);
+} 
 
+footerElement.innerHTML=`
+<div class="footer-content">
+    <div id="notice">
+        (C) Telekom Deutschland GmbH
+    </div>
+    <ul id="navigation">
+        <li><a href="https://static.magentacloud.de/licences/webui.htm" target="_blank" rel="noreferrer noopener">Open Source Lizenzen</a>
+        <li><a href="http://www.telekom.de/impressum" target="_blank" rel="noreferrer noopener">Impressum</a></li>
+        <li><a href="https://static.magentacloud.de/Datenschutz" target="_blank" rel="noreferrer noopener">Datenschutz</a>
+        </li>
+        <li><a href="https://cloud.telekom-dienste.de/hilfe" target="_blank" rel="noreferrer noopener">Hilfe und FAQ</a></li>
+    </ul>
+</div>
+`; 
+footerElement.setAttribute('role', 'contentinfo');
+footerElement.setAttribute('id', 'telekom-minimal-footer');
 
-
-
-
-
-
+if ( footerElement === null ) {
+    // add footer tag
+    footerElement = document.createElement('footer');
+    document.body.appendChild(footerElement);
+} 
