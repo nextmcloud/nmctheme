f443b0dbd09770fd8ea130b2e403d8fdbcb7a6b9
Bernd.Rederlechner@t-systems.com <bernd.rederlechner@t-systems.com>
2023-08-20 18:07:43 +0200

    Rework and optimize registration

--- lib/AppInfo/Application.php
+++ lib/AppInfo/Application.php
@@ -6,6 +6,7 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+
 namespace OCA\NMCTheme\AppInfo;
 
 use OC\AppFramework\DependencyInjection\DIContainer;
@@ -17,6 +18,7 @@ use OCA\NMCTheme\Themes\Magenta;
 use OCA\NMCTheme\Themes\MagentaDark;
 use OCA\NMCTheme\Themes\TeleNeoWebFont;
 use OCA\NMCTheme\URLGeneratorDecorator;
+use OC\URLGenerator;
 use OCA\Theming\Service\ThemesService;
 use OCA\Theming\Themes\DarkHighContrastTheme;
 use OCA\Theming\Themes\DarkTheme;
@@ -35,99 +37,115 @@ use OCP\IURLGenerator;
 use OCP\IUserSession;
 use OCP\L10N\IFactory;
 
-class Application extends App implements IBootstrap {
-	public const APP_ID = 'nmctheme';
+class Application extends App implements IBootstrap
+{
+    public const APP_ID = 'nmctheme';
 
-	public function __construct() {
-		parent::__construct(self::APP_ID);
-	}
+    public function __construct()
+    {
+        parent::__construct(self::APP_ID);
+    }
 
-	/**
-	 * Services are aquired by DI with priority for registrations for the own app.
-	 * As ThemesService is registered before or after nmctheme, and we want to
-	 * register NMCTheme version instead we either have to:
-	 * - register a theming app container early if nmctheme is registered before
-	 *   theming
-	 * - use the already registered container if theming is registered before
-	 *   nmctheme
-	 *
-	 * The "foreign" theming container can the be used for enforcing the registration
-	 * of the NMCThemesService factory method.
-	 */
-	public function getCapturedThemeingContainer() {
-		$appName = "theming";
-		try {
-			$container = \OC::$server->getRegisteredAppContainer($appName);
-		} catch (QueryException $e) {
-			$container = new DIContainer($appName);
-			\OC::$server->registerAppContainer($appName, $container);
-		}
+    /**
+     * Services are aquired by DI with priority for registrations for the own app.
+     * As ThemesService is registered before or after nmctheme, and we want to
+     * register NMCTheme version instead we either have to:
+     * - register a theming app container early if nmctheme is registered before
+     *   theming
+     * - use the already registered container if theming is registered before
+     *   nmctheme
+     *
+     * The "foreign" theming container can the be used for enforcing the registration
+     * of the NMCThemesService factory method.
+     */
+    public function otherAppContainer(string $appName)
+    {
+        try {
+            $container = \OC::$server->getRegisteredAppContainer($appName);
+        } catch (QueryException $e) {
+            $container = new DIContainer($appName);
+            \OC::$server->registerAppContainer($appName, $container);
+        }
 
-		return $container;
-	}
+        return $container;
+    }
 
-	/**
-	 * Decorate the IURLGenerator to intercept request for theming favicons.
-	 */
-	protected function registerURLGeneratorDecorator(IRegistrationContext $context) {
-		$this->getContainer()->getServer()->registerService(IURLGenerator::class, function ($c) {
-			return new URLGeneratorDecorator(
-				$this->getContainer()->getServer()->query(URLGenerator::class)
-			);
-		});
-	}
+    /**
+     * Decorate the IURLGenerator to intercept request for theming favicons.
+     */
+    protected function registerURLGeneratorDecorator(IRegistrationContext $context)
+    {
+        $this->getContainer()->getServer()->registerService(IURLGenerator::class, function ($c) {
+            return new URLGeneratorDecorator(
+                $this->getContainer()->getServer()->query(URLGenerator::class)
+            );
+        });
+    }
+
+    /**
+     * Decorate the L10N IFactory of server with the L10N theming factory
+     * so that any request for translation is either overridden by a value
+     * from this app or delegated to the original factory
+     */
+    protected function registerIFactoryDecorator(IRegistrationContext $context)
+    {
+        $this->getContainer()->getServer()->registerService(IFactory::class, function ($c) {
+            return new FactoryDecorator(
+                $c->get(IConfig::class),
+                $this->getContainer()->getServer()->query(Factory::class)
+            );
+        });
+    }
 
 
-	/**
-	 * Register all kind of decorators so that the theme is in control
-	 * of:
-	 * - the set of available themes
-	 * - the translations (to be overridden)
-	 * - the favicons
-	 */
-	public function register(IRegistrationContext $context): void {
 
-		/**
-		 * Register decorators softly extending Nextcloud upstream standard
-		 */
+    /**
+     * Register all kind of decorators so that the theme is in control
+     * of:
+     * - the set of available themes
+     * - the translations (to be overridden)
+     * - the favicons
+     */
+    public function register(IRegistrationContext $context): void
+    {
 
-		// explicitly register own NMCThemesManager to override the Nextcloud standard
-		$this->getCapturedThemeingContainer()->registerService(ThemesService::class, function ($c) {
-			return new NMCThemesService(
-				$c->get(IUserSession::class),
-				$c->get(IConfig::class),
-				$c->get(Magenta::class),
-				//[$c->get(MagentaDark::class)],    // FIXME
-				[],
-				[$c->get(TeleNeoWebFont::class)],
-				$c->get(DefaultTheme::class),   // the rest is overhead due to undefined interface (yet)
-				$c->get(LightTheme::class),
-				$c->get(DarkTheme::class),
-				$c->get(HighContrastTheme::class),
-				$c->get(DarkHighContrastTheme::class),
-				$c->get(DyslexiaFont::class)
-			);
-		});
-		$this->registerIFactoryDecorator($context);
-	
-		// the listener is helpful to enforce theme constraints and inject additional parts
-		// $context->registerEventListener(BeforeTemplateRenderedEvent::class, BeforeTemplateRenderedListener::class);
-	}
-	
-	/**
-	 * Decorate the L10N IFactory of server with the L10N theming factory
-	 * so that any request for translation is either overridden by a value
-	 * from this app or delegated to the original factory
-	 */
-	protected function registerIFactoryDecorator(IRegistrationContext $context) {
-		$this->getContainer()->getServer()->registerService(IFactory::class, function ($c) {
-			return new FactoryDecorator(
-				$c->get(IConfig::class),
-				$this->getContainer()->getServer()->query(Factory::class)
-			);
-		});
-	}
+        /**
+         * Register decorators softly extending Nextcloud upstream standard
+         */
 
-	public function boot(IBootContext $context): void {
-	}
+        // explicitly register own NMCThemesManager to override the Nextcloud standard
+        $this->otherAppContainer("theming")->registerService(ThemesService::class, function ($c) {
+            return new NMCThemesService(
+                $c->get(IUserSession::class),
+                $c->get(IConfig::class),
+                $c->get(Magenta::class),
+                //[$c->get(MagentaDark::class)],    // FIXME
+                [],
+                [$c->get(TeleNeoWebFont::class)],
+                $c->get(DefaultTheme::class),   // the rest is overhead due to undefined interface (yet)
+                $c->get(LightTheme::class),
+                $c->get(DarkTheme::class),
+                $c->get(HighContrastTheme::class),
+                $c->get(DarkHighContrastTheme::class),
+                $c->get(DyslexiaFont::class)
+            );
+        });
+        
+        // intercept requests for favicons to enforce own behavior
+        $this->registerURLGeneratorDecorator($context);
+
+        // intercept requests for translations, theme specific translations have prio
+        $this->registerIFactoryDecorator($context);
+
+        /**
+         * Add listeners that can inject additional information or scripts before rendering
+         */
+
+        // the listener is helpful to enforce theme constraints and inject additional parts
+        $context->registerEventListener(BeforeTemplateRenderedEvent::class, BeforeTemplateRenderedListener::class);
+    }
+
+    public function boot(IBootContext $context): void
+    {
+    }
 }
---
+++ tests/unit/RegistrationsTest.php
@@ -0,0 +1,50 @@
+<?php
+/*
+ * @copyright Copyright (c) 2021 T-Systems International
+ *
+ * @author Bernd Rederlechner <bernd.rederlechner@t-systems.com>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ */
+
+declare(strict_types=1);
+
+use OCA\NMCTheme\AppInfo\Application;
+use OC\AppFramework\Bootstrap\Coordinator;
+
+use OCA\NMCTheme\Service\NMCThemesService;
+use OCA\Theming\Service\ThemesService;
+
+use OCP\IURLGenerator;
+use OCA\NMCTheme\URLGeneratorDecorator;
+
+use OCP\L10N\IFactory;
+use OCA\NMCTheme\L10N\FactoryDecorator;
+
+use PHPUnit\Framework\TestCase;
+
+class RegistrationsTest extends TestCase {
+	public function setUp() :void {
+		parent::setUp();
+
+		$this->app = new Application();
+		$coordinator = \OC::$server->get(Coordinator::class);
+		$this->app->register($coordinator->getRegistrationContext()->for('nmctheme'));
+	}
+
+	public function testThemingOverrideRegistration() :void {
+		$themesService = \OC::$server->getRegisteredAppContainer("theming")->get(ThemesService::class);
+		$this->assertInstanceOf(NMCThemesService::class, $themesService, "FATAL: NMCTheme is not registered!");
+	}
+
+    public function testDecoratedURLGenerator() :void {
+		$urlGenerator = $this->app->getContainer()->get(IURLGenerator::class);
+		$this->assertInstanceOf(URLGeneratorDecorator::class, $urlGenerator , "FATAL: URLGeneratorDecorator failed to register!");
+	}
+
+    public function testDecoratedIFactoryL10N() :void {
+		$factoryDecorator = $this->app->getContainer()->get(IFactory::class);
+		$this->assertInstanceOf(FactoryDecorator::class, $factoryDecorator , "FATAL: L10N FactoryDecorator failed to register!");
+	}
+}
