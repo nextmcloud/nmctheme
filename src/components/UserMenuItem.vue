97dc49a5caca279fdfaf126fea90e3798ee1ba98
Mauro Mura <Mauro-Efisio.Mura@t-systems.com>
2023-09-06 13:54:46 +0200

    Settings app styling and scripts

---
+++ src/components/UserMenuItem.vue
@@ -0,0 +1,8 @@
+<template>
+	<li id="account" class="menu-entry" type="settings">
+		<a href="/index.php/settings/user/account">
+			<img src="/core/img/actions/user.svg">
+			<span>Account Settings</span>
+		</a>
+	</li>
+</template>
