01227b0193b86833bc501ab32a5100b1b7a603c6
Mauro Mura <Mauro-Efisio.Mura@t-systems.com>
2023-09-07 11:57:31 +0200

    updated user menu

--- src/components/UserMenuItem.vue
+++ src/components/UserMenuItem.vue
@@ -1,8 +1,34 @@
 <template>
-	<li id="account" class="menu-entry" type="settings">
-		<a href="/index.php/settings/user/account">
-			<img src="/core/img/actions/user.svg">
-			<span>Account Settings</span>
+	<li :id="id" class="menu-entry">
+		<a :href="url" :target="target">
+			<img :src="icon">
+			<span>{{ t('nmcsettings', name) }}</span>
 		</a>
 	</li>
 </template>
+
+<script>
+import { translate } from '@nextcloud/l10n'
+
+export default {
+	props: {
+		itemId: String,
+		itemName: String,
+		itemIcon: String,
+		itemUrl: String,
+		itemTarget: String,
+	},
+	data() {
+		return {
+			id: this.itemId,
+			name: this.itemName,
+			icon: this.itemIcon,
+			url: this.itemUrl,
+			target: this.itemTarget,
+		}
+	},
+	methods: {
+		t: translate,
+	},
+}
+</script>
--- src/js/nmcsettings.js
+++ src/js/nmcsettings.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import UserMenuItem from '../components/UserMenuItem.vue'
 
+const menuItems = [{
+	itemId: 'customer-center',
+	itemName: 'Customer Center',
+	itemIcon: '/customapps/nmctheme/img/settings/img/kundencenter.svg',
+	itemUrl: 'https://www.telekom.de/mein-kundencenter',
+	itemTarget: '_blank',
+}, {
+	itemId: 'help',
+	itemName: 'Help & FAQ',
+	itemIcon: '/customapps/nmctheme/img/settings/img/help.svg',
+	itemUrl: 'https://cloud.telekom-dienste.de/hilfe',
+	itemTarget: '_blank',
+}, {
+	itemId: 'settings',
+	itemName: 'Account Settings',
+	itemIcon: '/customapps/nmctheme/img/settings/img/admin.svg',
+	itemUrl: '/index.php/settings/user/account',
+	itemTarget: '_self',
+}]
+
 const UserMenuItemView = Vue.extend(UserMenuItem)
-const View = new UserMenuItemView()
 
 window.addEventListener('DOMContentLoaded', function() {
 
@@ -12,11 +31,20 @@ window.addEventListener('DOMContentLoaded', function() {
 		}
 	})
 
+	const logoutIcon = document.querySelector('nav.user-menu__nav img')
+	logoutIcon.src = '/customapps/nmctheme/img/actions/logout.svg'
+
 	const userMenu = document.querySelector('nav.user-menu__nav ul')
-	const listItemAccount = document.createElement('div')
-	listItemAccount.id = 'account-settings'
-	userMenu.prepend(listItemAccount)
-	View.$mount('#account-settings')
+
+	menuItems.forEach(function(menuItem) {
+		const menuItemElement = document.createElement('div')
+		menuItemElement.id = 'nmcsettings-menu-' + menuItem.itemId
+		userMenu.prepend(menuItemElement)
+
+		new UserMenuItemView({
+			propsData: menuItem,
+		}).$mount('#nmcsettings-menu-' + menuItem.itemId)
+	})
 
 	const settingsBody = document.getElementById('body-settings')
 	if (!settingsBody) return
@@ -34,10 +62,10 @@ window.addEventListener('DOMContentLoaded', function() {
 
 	// removes app navigation elements in NC25
 	document.querySelectorAll('#app-navigation li a').forEach(function(element) {
-		const href = element.attributes['href'].value
-		const user = "/user";
-		const account = "/account";
-		const session = "/session";
+		const href = element.href
+		const user = '/user'
+		const account = '/account'
+		const session = '/session'
 
 		if (href.includes(user) && !href.includes(account) && !href.includes(session)) {
 			element.parentElement.remove()
