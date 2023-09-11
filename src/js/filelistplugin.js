1367a9865beada4eb5cd5c56cb796dd108def93f
Mauro Mura <Mauro-Efisio.Mura@t-systems.com>
2023-09-11 14:50:21 +0200

    fixed js and actions alignment

--- src/js/filelistplugin.js
+++ src/js/filelistplugin.js
@@ -3,9 +3,8 @@
 	const FilesPlugin = {
 
 		attach(fileList) {
-			var self = this;
 			const actionHandler = () => {
-				this._onClickCancelSelected(fileList);
+				this._onClickCancelSelected(fileList)
 			}
 
 			fileList.registerMultiSelectFileAction({
@@ -17,98 +16,95 @@
 			})
 
 			fileList.updateSelectionSummary = function() {
-				var self = this;
-				var summary = fileList._selectionSummary.summary;
-				var selection;
+				const self = this
+				const summary = fileList._selectionSummary.summary
+				let selection
 
-				var showHidden = !!fileList._filesConfig.show_hidden;
+				const showHidden = !!fileList._filesConfig.show_hidden
 
-				console.log(fileList);
+				const fileTable = fileList.$table
+				fileTable.find('.column-selection > label > span').text(t('files', 'All'))
 
-				var fileTable = fileList.$table;
-				fileTable.find('.column-selection > label > span').text(t('files', 'All'));
-				
 				if (summary.totalFiles === 0 && summary.totalDirs === 0) {
-					fileTable.find('.column-name a.name > span:first').text(t('files', 'Name'));
-					fileTable.find('table').removeClass('multiselect');
-					fileTable.find('.column-size').removeClass('hidden');
-					fileTable.find('.column-mtime').removeClass('hidden');
-					fileTable.find('.selectedActions').addClass('hidden');
+					fileTable.find('.column-name a.name > span:first').text(t('files', 'Name'))
+					fileTable.find('table').removeClass('multiselect')
+					fileTable.find('.column-size').removeClass('hidden')
+					fileTable.find('.column-mtime').removeClass('hidden')
+					fileTable.find('.selectedActions').addClass('hidden')
 				} else {
-					fileTable.find('.column-size').addClass('hidden');
-					fileTable.find('.column-mtime').addClass('hidden');
-					fileTable.find('.selectedActions').removeClass('hidden');
+					fileTable.find('.column-size').addClass('hidden')
+					fileTable.find('.column-mtime').addClass('hidden')
+					fileTable.find('.selectedActions').removeClass('hidden')
 
-					fileList.fileMultiSelectMenu.show(self);
+					fileList.fileMultiSelectMenu.show(self)
 
-					var directoryInfo = n('files', '%n folder', '%n folders', summary.totalDirs);
-					var fileInfo = n('files', '%n file', '%n files', summary.totalFiles);
+					const directoryInfo = n('files', '%n folder', '%n folders', summary.totalDirs)
+					const fileInfo = n('files', '%n file', '%n files', summary.totalFiles)
 
 					if (summary.totalDirs > 0 && summary.totalFiles > 0) {
-						var selectionVars = {
+						const selectionVars = {
 							dirs: directoryInfo,
-							files: fileInfo
-						};
-						selection = t('files', '{dirs} and {files}', selectionVars);
+							files: fileInfo,
+						}
+						selection = t('files', '{dirs} and {files}', selectionVars)
 					} else if (summary.totalDirs > 0) {
-						selection = directoryInfo;
+						selection = directoryInfo
 					} else {
-						selection = fileInfo;
+						selection = fileInfo
 					}
 
 					if (!showHidden && summary.totalHidden > 0) {
-						var hiddenInfo = n('files', 'including %n hidden', 'including %n hidden', summary.totalHidden);
-						selection += ' (' + hiddenInfo + ')';
+						const hiddenInfo = n('files', 'including %n hidden', 'including %n hidden', summary.totalHidden)
+						selection += ' (' + hiddenInfo + ')'
 					}
 
 					if (summary.totalSize > 0) {
-						selection += ' (' + OC.Util.humanFileSize(summary.totalSize) + ')';
+						selection += ' (' + OC.Util.humanFileSize(summary.totalSize) + ')'
 					}
 
-					$('.column-name a.name > span:first').text(selection);
-
-					$('table').addClass('multiselect');
+					fileTable.find('.column-name a.name > span:first').text(selection)
+					fileTable.find('table').addClass('multiselect')
 
 					if (fileList.fileMultiSelectMenu) {
-						fileList.fileMultiSelectMenu.toggleItemVisibility('download', fileList.isSelectedDownloadable());
-						fileList.fileMultiSelectMenu.toggleItemVisibility('delete', fileList.isSelectedDeletable());
-						fileList.fileMultiSelectMenu.toggleItemVisibility('copyMove', fileList.isSelectedCopiable());
+						fileList.fileMultiSelectMenu.toggleItemVisibility('download', fileList.isSelectedDownloadable())
+						fileList.fileMultiSelectMenu.toggleItemVisibility('delete', fileList.isSelectedDeletable())
+						fileList.fileMultiSelectMenu.toggleItemVisibility('copyMove', fileList.isSelectedCopiable())
 						if (fileList.isSelectedCopiable()) {
 							if (fileList.isSelectedMovable()) {
-								fileList.fileMultiSelectMenu.updateItemText('copyMove', t('files', 'Move or copy'));
+								fileList.fileMultiSelectMenu.updateItemText('copyMove', t('files', 'Move or copy'))
 							} else {
-								fileList.fileMultiSelectMenu.updateItemText('copyMove', t('files', 'Copy'));
+								fileList.fileMultiSelectMenu.updateItemText('copyMove', t('files', 'Copy'))
 							}
 						} else {
-							fileList.fileMultiSelectMenu.toggleItemVisibility('copyMove', false);
+							fileList.fileMultiSelectMenu.toggleItemVisibility('copyMove', false)
 						}
 					}
 
 					if (fileList.fileMultipleSelectionMenu) {
-						fileList.fileMultipleSelectionMenu.toggleItemVisibility('download', fileList.isSelectedDownloadable());
-						fileList.fileMultipleSelectionMenu.toggleItemVisibility('delete', fileList.isSelectedDeletable());
-						fileList.fileMultipleSelectionMenu.toggleItemVisibility('copyMove', fileList.isSelectedCopiable());
+						fileList.fileMultipleSelectionMenu.toggleItemVisibility('download', fileList.isSelectedDownloadable())
+						fileList.fileMultipleSelectionMenu.toggleItemVisibility('delete', fileList.isSelectedDeletable())
+						fileList.fileMultipleSelectionMenu.toggleItemVisibility('copyMove', fileList.isSelectedCopiable())
 						if (fileList.isSelectedCopiable()) {
 							if (fileList.isSelectedMovable()) {
-								fileList.fileMultipleSelectionMenu.updateItemText('copyMove', t('files', 'Move or copy'));
+								fileList.fileMultipleSelectionMenu.updateItemText('copyMove', t('files', 'Move or copy'))
 							} else {
-								fileList.fileMultipleSelectionMenu.updateItemText('copyMove', t('files', 'Copy'));
+								fileList.fileMultipleSelectionMenu.updateItemText('copyMove', t('files', 'Copy'))
 							}
 						} else {
-							fileList.fileMultipleSelectionMenu.toggleItemVisibility('copyMove', false);
+							fileList.fileMultipleSelectionMenu.toggleItemVisibility('copyMove', false)
 						}
 					}
 				}
 			}
 		},
 
-		_onClickCancelSelected: function (fileList) {
-			fileList._selectedFiles = {};
-			fileList._selectionSummary.clear();
-			fileTable.find('input').prop('checked', false);
-			fileTable.find('td.selection > .selectCheckBox:visible').closest('tr').toggleClass('selected', false);
-			fileList.updateSelectionSummary();
-		}
+		_onClickCancelSelected(fileList) {
+			fileList._selectedFiles = {}
+			fileList._selectionSummary.clear()
+			fileList.$table.find('input').prop('checked', false)
+			fileList.$table.find('td.selection > .selectCheckBox:visible').closest('tr').toggleClass('selected', false)
+			fileList.updateSelectionSummary()
+		},
 	}
 
 	OC.Plugins.register('OCA.Files.FileList', FilesPlugin)
