ac9c31c4ddd6ef10be816f8f11fcd6019e041687
Mauro Mura <Mauro-Efisio.Mura@t-systems.com>
2023-10-19 16:38:42 +0200

    hide all menus on file creation

--- src/js/newfilemenuplugin.js
+++ src/js/newfilemenuplugin.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', function() {
 	const NewFileMenuPlugin = {
 
 		attach(menu) {
+			const that = this
 
 			menu.render = function() {
 				const self = this
@@ -25,6 +26,7 @@ window.addEventListener('DOMContentLoaded', function() {
 					$(element).removeClass('menuitem').addClass('customitem')
 
 					$(element).on('click', function(event) {
+
 						let $target = $(event.target) // eslint-disable-line
 	
 						if (!$target.hasClass('menuitem')) {
@@ -37,10 +39,12 @@ window.addEventListener('DOMContentLoaded', function() {
 
 						if(filetype === 'file') {
 							Promise.all([self.fileList.createFile(uniqueName)]).then(() => {
+								that._hideAllMenus(self.fileList)
 								self.fileList.rename(uniqueName)
 							})
 						} else if(filetype === 'folder') {
 							Promise.all([self.fileList.createDirectory(uniqueName)]).then(() => {
+								that._hideAllMenus(self.fileList)
 								self.fileList.rename(uniqueName)
 							})
 						}
@@ -50,6 +54,13 @@ window.addEventListener('DOMContentLoaded', function() {
 
 			// remove 'Set up templates folder' option
 			menu.removeMenuEntry('template-init')
+		},
+
+		_hideAllMenus(fileList) {
+			fileList.$el.find('.column-menu').addClass('hidden')
+			fileList.$el.find('.column-actions').addClass('hidden')
+			OC.hideMenus()
+			OCA.Files.Sidebar.close()
 		},
 	}
 
