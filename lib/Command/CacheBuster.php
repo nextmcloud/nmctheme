4e122c3fe67a221879122044e89a4191489feab7
Bernd.Rederlechner@t-systems.com <bernd.rederlechner@t-systems.com>
2023-12-11 11:51:22 +0100

    Clean up code

--- lib/Command/CacheBuster.php
+++ lib/Command/CacheBuster.php
@@ -11,50 +11,47 @@ declare(strict_types=1);
 
 namespace OCA\NMCTheme\Command;
 
-use Exception;
-
-use OCA\Theming\ThemingDefaults;
-use OCA\Theming\Util;
-
 use \Symfony\Component\Console\Command\Command;
-use Symfony\Component\Console\Input\InputArgument;
+
+use Exception;
+use OCA\Theming\ThemingDefaults;
+
+use OCA\Theming\Util;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Question\ConfirmationQuestion;
-
 
 class CacheBuster extends Command {
 
-    protected ThemingDefaults $themingDefaults;
-    protected Util $themeUtil;
+	protected ThemingDefaults $themingDefaults;
+	protected Util $themeUtil;
 
 	public function __construct(ThemingDefaults $themeingDefaults,
-                                Util $themeUtil) {
+		Util $themeUtil) {
 		parent::__construct();
-        $this->themeingDefaults = $themeingDefaults;
-        $this->themeUtil = $themeUtil;
+		$this->themeingDefaults = $themeingDefaults;
+		$this->themeUtil = $themeUtil;
 	}
 
 	protected function configure() {
 		$this
 			->setName('nmctheme:cachebuster')
 			->setDescription('Show/modify cachebuster')
-            ->setHelp('Show/bust the cachebuster query parameter to remote bust browser caches')
-            ->addOption('bust', 'b', InputOption::VALUE_OPTIONAL, 'Bust all user browser caches', false);
+			->setHelp('Show/bust the cachebuster query parameter to remote bust browser caches')
+			->addOption('bust', 'b', InputOption::VALUE_OPTIONAL, 'Bust all user browser caches', false);
 		parent::configure();
 	}
 
 	protected function execute(InputInterface $input, OutputInterface $output) {
 		try {
-            $doBust = $input->getOption('bust');
-            if ($doBust === false) {
-                $output->writeln('CacheBusterValue=' . $this->themeUtil->getCacheBuster());
-            } else {
-                $output->writeln('CacheBusterOldValue=' . $this->themeUtil->getCacheBuster());
-                $this->themeingDefaults->increaseCacheBuster();
-                $output->writeln('CacheBusterNewValue=' . $this->themeUtil->getCacheBuster());
-            }
+			$doBust = $input->getOption('bust');
+			if ($doBust === false) {
+				$output->writeln('CacheBusterValue=' . $this->themeUtil->getCacheBuster());
+			} else {
+				$output->writeln('CacheBusterOldValue=' . $this->themeUtil->getCacheBuster());
+				$this->themeingDefaults->increaseCacheBuster();
+				$output->writeln('CacheBusterNewValue=' . $this->themeUtil->getCacheBuster());
+			}
 
 		} catch (Exception $e) {
 			$output->writeln('Error: ' . $e->getMessage());
