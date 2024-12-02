3823e0bc51737b8af74c3cbf36c2b8818f69ea3b
Mauro Mura <Mauro-Efisio.Mura@t-systems.com>
2024-12-02 14:32:46 +0100

    fixed union type issue

--- src/store/userconfig.ts
+++ src/store/userconfig.ts
@@ -51,10 +51,9 @@ export const useUserConfigStore = function(...args) {
 	// Make sure we only register the listeners once
 	if (!userConfigStore._initialized) {
 		subscribe('files:config:updated', (params) => {
-			if (params) {
-				userConfigStore.onUpdate(params.key, params.value)
-			}
-		})
+			const userParams = params as { key: string; value: boolean }
+			userConfigStore.onUpdate(userParams.key, userParams.value)
+fixed		})
 		userConfigStore.onUpdate('_initialized', true)
 	}
 
