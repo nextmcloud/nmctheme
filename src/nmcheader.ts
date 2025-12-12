cc2475a4443d46a2233e74a0fd4336757bd06419
memurats <Mauro-Efisio.Mura@t-systems.com>
2025-12-12 13:01:06 +0100

    fix welcome popup menu entry

--- src/nmcheader.ts
+++ src/nmcheader.ts
@@ -20,11 +20,6 @@ const menuItems = [{
 	name: t(app, 'Customer center'),
 	url: 'https://www.telekom.de/mein-kundencenter',
 	target: '_blank',
-}, {
-	id: 'nmc_welcome_popup-about',
-	name: t(app, 'News'),
-	url: '#',
-	target: '_self',
 }]
 
 window.addEventListener('DOMContentLoaded', function() {
