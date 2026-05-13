2e1a89fded855767406b16d7ea148e485550b3f3
memurats <Mauro-Efisio.Mura@t-systems.com>
2026-05-13 11:49:56 +0200

    fix webview footer when viewer is active

--- src/nmcfooter.ts
+++ src/nmcfooter.ts
@@ -9,29 +9,67 @@ const faq = t(app, 'Faq')
 
 const footerContent = `
 <div class="footer-content">
-    <div id="notice">${copyright}</div>
-    <ul id="navigation">
-        <li><a href="https://static.magentacloud.de/licences/webui.htm" target="_blank" rel="noreferrer noopener">${opensource}</a>
-        <li><a href="https://www.telekom.de/impressum" target="_blank" rel="noreferrer noopener">${impressum}</a></li>
-        <li><a href="https://static.magentacloud.de/Datenschutz" target="_blank" rel="noreferrer noopener">${dataprotection}</a>
-        </li>
-        <li><a href="https://cloud.telekom-dienste.de/hilfe" target="_blank" rel="noreferrer noopener">${faq}</a></li>
-    </ul>
+	<div id="notice">${copyright}</div>
+	<ul id="navigation">
+		<li>
+			<a href="https://static.magentacloud.de/licences/webui.htm" target="_blank" rel="noreferrer noopener">
+				${opensource}
+			</a>
+		</li>
+		<li>
+			<a href="https://www.telekom.de/impressum" target="_blank" rel="noreferrer noopener">
+				${impressum}
+			</a>
+		</li>
+		<li>
+			<a href="https://static.magentacloud.de/Datenschutz" target="_blank" rel="noreferrer noopener">
+				${dataprotection}
+			</a>
+		</li>
+		<li>
+			<a href="https://cloud.telekom-dienste.de/hilfe" target="_blank" rel="noreferrer noopener">
+				${faq}
+			</a>
+		</li>
+	</ul>
 </div>
 `
+
 const footerRole = 'contentinfo'
 const footerId = 'telekom-minimal-footer'
 
-let footerElement = document.querySelector('body footer')
-if (footerElement === null) {
-	// add footer tag
-	footerElement = document.createElement('footer')
-	footerElement.innerHTML = footerContent
-	footerElement.setAttribute('role', footerRole)
-	footerElement.setAttribute('id', footerId)
-	document.body.appendChild(footerElement)
-} else {
-	footerElement.innerHTML = footerContent
-	footerElement.setAttribute('role', footerRole)
-	footerElement.setAttribute('id', footerId)
+const updateFooterVisibility = () => {
+	const footer = document.getElementById(footerId)
+
+	if (!footer) {
+		return
+	}
+
+	const viewerExists = document.getElementById('viewer') !== null
+	footer.style.display = viewerExists ? 'none' : ''
 }
+
+// Footer erstellen/aktualisieren
+let footerElement = document.querySelector('body footer')
+
+if (footerElement === null) {
+	footerElement = document.createElement('footer')
+	document.body.appendChild(footerElement)
+}
+
+footerElement.innerHTML = footerContent
+footerElement.setAttribute('role', footerRole)
+footerElement.setAttribute('id', footerId)
+
+// Beobachte DOM Änderungen
+const observer = new MutationObserver(() => {
+	updateFooterVisibility()
+})
+
+observer.observe(document.body, {
+	childList: true,
+	subtree: true,
+})
+
+// Initial prüfen
+updateFooterVisibility()
